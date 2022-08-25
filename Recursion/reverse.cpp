cfce6541ae09bb7250f6fdd1f10093f3eb1c5818
Neetu Kumari <chaudharyneetu749@gmail.com>
2022-08-25 20:07:32 +0530

    Reverse an array
    C++ program to reverse an array using recursion

---
+++ Recursion/reverse.cpp
@@ -0,0 +1,26 @@
+// Reverse an array:
+// 1,2,3,4,2
+// Neetu Kumari
+
+#include <iostream>
+using namespace std;
+
+void f(int i, int arr[], int n){
+    if(i>=n/2)
+    return;
+    swap(arr[i], arr[n-i-1]);
+    f(i+1,arr, n);
+}
+int main(){
+    int n;
+    cout<<"enter the num:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    f(0,arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
